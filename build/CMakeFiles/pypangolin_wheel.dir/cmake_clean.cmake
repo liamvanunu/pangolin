5cbba1f3776be10dfc7abf30a26616ce1f51105f
liam <liamvanunu1231@gmail.com>
2023-04-10 17:02:34 +0300

    Removed build
    Signed-off-by: liam <liamvanunu1231@gmail.com>

--- build/CMakeFiles/pypangolin_wheel.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/pypangolin_wheel"
-  "pypangolin-0.8-cp38-cp38-linux_x86_64.whl"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/pypangolin_wheel.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
